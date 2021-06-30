90437871409882975058d5c95d48c5ead88fa203
John <johncheung1997@gmail.com>
2021-06-30 13:38:29 -0400

    change api url to https

--- default.js
+++ default.js
@@ -1,6 +1,6 @@
 function defaultPage() {
 
-    fetch("http://ddragon.leagueoflegends.com/cdn/11.13.1/data/en_US/champion.json")
+    fetch("https://ddragon.leagueoflegends.com/cdn/11.13.1/data/en_US/champion.json/")
         .then(res => res.json())
         .then(data => Object.values(data.data).forEach(champ => {
             let list = document.getElementById("champion-list")
@@ -9,10 +9,10 @@ function defaultPage() {
             `
             ${champ.name}
             <img src=${championImg(champ.id)}>
-            <a href="https://u.gg/lol/champions/${champ.name}/build">
+            <a href="https://u.gg/lol/champions/${champ.name}/build/">
                 Summoner's Rift
             </a>
-            <a href="https://u.gg/lol/champions/aram/${champ.name}-aram">
+            <a href="https://u.gg/lol/champions/aram/${champ.name}-aram/">
                 ARAM
             </a>
             `
